421b60ef7282c71fcc6694668a78cb0fbb8c3924
ugesh-2603 <75109057+ugesh-2603@users.noreply.github.com>
2020-12-07 11:24:55 +0530

    Add files via upload

---
+++ C# Day 2 Ex 1.cs
@@ -0,0 +1,19 @@
+using System;
+namespace internship_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(remove_cahr("I am a monkey", 5));
+        }
+
+        public static int test(int x, int y, int z)
+        {
+            return str.Remove(n,1);
+
+        }
+    }
+}
+    
+
