f9ba711462c7a268df5cf9f69041ee29a5354f56
ugesh-2603 <75109057+ugesh-2603@users.noreply.github.com>
2020-12-07 11:05:45 +0530

    Add files via upload

---
+++ C# Day1 Ex1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace internship_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("ugesh kumar");
+        }
+    }
+}
